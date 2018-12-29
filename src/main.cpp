d873e9f90bd19deb9174e1f96816da0e97d7d0c2
3jackattack3 <3jackattack3@gmail.com>
2018-12-29 01:20:05 -0500

    added radio controls!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF69.h>
+
+#define RF69_FREQ 900.0
+
+  #define RFM69_CS      8
+  #define RFM69_INT     3
+  #define RFM69_RST     4
+  #define LED           13
+
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
 int button1 = 12;  //sets light to single color mode
 int button2 = 11;
@@ -10,6 +21,29 @@ void singleColor(int color);
 void twoColorSpectrum(int color1, int color2);
 
 void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(RFM69_RST, OUTPUT);
+    digitalWrite(RFM69_RST, LOW);
+
+      // manual reset
+    digitalWrite(RFM69_RST, HIGH);
+    delay(10);
+    digitalWrite(RFM69_RST, LOW);
+    delay(10);
+
+    if (!rf69.init()) {
+      //Serial.println("RFM69 radio init failed");
+      while (1);
+    }
+    Serial.println("RFM69 radio init OK!");
+    if (!rf69.setFrequency(RF69_FREQ)) {
+      //Serial.println("setFrequency failed");
+    }
+    rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+    uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                      0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+    rf69.setEncryptionKey(key);
+
     pinMode(button1, INPUT);
     pinMode(button2, INPUT);
     pinMode(button3, INPUT);
@@ -62,11 +96,49 @@ void loop() {
 }
 
 void singleColor(int color){
-    //send mode and color
-    Serial.println(color);
+    char radiopacket[20];
+    char temp[5];
+    String tempWord = "";
+    int mode = 1;
+
+    itoa((int) mode, temp, 10);
+    tempWord += temp;
+    tempWord += "* ";
+
+    itoa((int) color, temp, 10);
+    tempWord += temp;
+    tempWord += "* ";
+
+    tempWord += "* ";
+    tempWord.toCharArray(radiopacket, 20);
+
+    Serial.println(radiopacket);
+
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
 }
 
 void twoColorSpectrum(int color1, int color2){
-    //send mode and two colors
-    Serial.println(color1);
+    char radiopacket[20];
+    char temp[5];
+    String tempWord = "";
+    int mode = 2;
+
+    itoa((int) mode, temp, 10);
+    tempWord = temp;
+    tempWord += "* ";
+
+    itoa((int) color1, temp, 10);
+    tempWord += temp;
+    tempWord += "* ";
+
+    itoa((int) color2, temp, 10);
+    tempWord += temp;
+    tempWord += "* ";
+    tempWord.toCharArray(radiopacket, 20);
+
+    Serial.println(radiopacket);
+
+    rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+    rf69.waitPacketSent();
 }
